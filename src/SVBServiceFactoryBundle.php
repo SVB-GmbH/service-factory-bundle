a95bb652aaa11f4da30d421ec9f113265d34e09f
svb-technik <99185971+svb-technik@users.noreply.github.com>
2024-01-05 09:35:29 +0100

    Made compatible with Symfony 6.x

--- src/SVBServiceFactoryBundle.php
+++ src/SVBServiceFactoryBundle.php
@@ -5,11 +5,12 @@ namespace SVB\ServiceFactoryBundle;
 use SVB\ServiceFactoryBundle\DependencyInjection\ServiceFactoryCompilerPass;
 use SVB\ServiceFactoryBundle\DependencyInjection\ServiceFactoryContainerExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SVBServiceFactoryBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new ServiceFactoryContainerExtension();
